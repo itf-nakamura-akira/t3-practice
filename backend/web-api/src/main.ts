c633d165703daf4aff5762d7424b3dc18fcbf94d
itf-nakamura-akira <nakamura.akira@itfllc.co.jp>
2023-01-21 15:23:28 +0000

    CORSの設定追加

--- backend/web-api/src/main.ts
+++ backend/web-api/src/main.ts
@@ -2,7 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+    // サーバーインスタンス生成
     const app = await NestFactory.create(AppModule);
+
+    // 設定
+    app.setGlobalPrefix('api');
+    app.enableCors({
+        origin: 'localhost:3000',
+        allowedHeaders: ['Content-Type', 'Authorization'],
+        methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    });
+
+    // 起動
     await app.listen(3000);
 }
 bootstrap();
