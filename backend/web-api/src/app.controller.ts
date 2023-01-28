b253fe66d8e94c8634e25b9ea734ae239221deba
itf-nakamura-akira <nakamura.akira@itfllc.co.jp>
2023-01-28 11:18:00 +0000

    debug環境の構築

--- backend/web-api/src/app.controller.ts
+++ backend/web-api/src/app.controller.ts
@@ -1,18 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { AppConfigService } from './common/services/app-config/app-config.service';
 
 @Controller()
 export class AppController {
-    constructor(private readonly appConfigService: AppConfigService, private readonly appService: AppService) {}
+    constructor(private readonly appService: AppService) {}
 
     @Get()
-    getHello(): { port: number } {
-        console.log(process.env.NODE_ENV + ': ' + this.appConfigService.serverPort);
-
-        return {
-            port: this.appConfigService.serverPort,
-        };
-        // return this.appService.getHello();
+    getHello(): string {
+        return this.appService.getHello();
     }
 }
