9dc708a725c58027f2286872513e4de6a9fdf1c1
itf-nakamura-akira <nakamura.akira@itfllc.co.jp>
2023-01-28 12:50:47 +0000

    expressからfastifyへ移行

--- backend/web-api/src/main.ts
+++ backend/web-api/src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import { AppConfigService } from './common/services/app-config/app-config.service';
 
 async function bootstrap() {
     // サーバーインスタンス生成
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter({ logger: true }));
 
     // 設定
     const appConfigService: AppConfigService = app.get(AppConfigService);
@@ -17,6 +18,6 @@ async function bootstrap() {
     });
 
     // 起動
-    await app.listen(appConfigService.serverPort);
+    await app.listen(appConfigService.serverPort, '0.0.0.0');
 }
 bootstrap();
